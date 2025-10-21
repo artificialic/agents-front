dd5063a5e02d6bf62d638257791eca5de6c6af19
Carl Lewis Castillo <carllewiscastillo@gmail.com>
2025-10-20 20:27:35 -0400

    Update campaign-contacts-dashboard.tsx

--- components/modules/campaign/campaign-contacts-dashboard.tsx
+++ components/modules/campaign/campaign-contacts-dashboard.tsx
@@ -177,6 +177,8 @@ export default function CampaignContactsDashboard({
         return 'bg-red-100 text-red-800';
       case 'processing':
         return 'bg-blue-100 text-blue-800';
+      case 'working':
+        return 'bg-blue-100 text-blue-800';
       default:
         return 'bg-gray-100 text-gray-800';
     }
