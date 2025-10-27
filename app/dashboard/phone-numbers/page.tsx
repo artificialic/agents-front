648da272368cf4484629f1b5e52ffa17259f09e3
Carl Lewis Castillo <carllewiscastillo@gmail.com>
2025-10-27 12:51:35 -0400

    fix: resolve problem with import default

--- app/dashboard/phone-numbers/page.tsx
+++ app/dashboard/phone-numbers/page.tsx
@@ -9,7 +9,7 @@ import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Button } from '@/components/ui/button';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
-import { CustomAlertDialog } from '@/components';
+import { CustomAlertDialog } from '@/components/custom-alert-dialog';
 
 interface AgentLocal {
   id: string;
